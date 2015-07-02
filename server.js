bd870d5a12fb770ed8e9457e222c780d06bb8d7a
lianxu <qinhui.cqh@alibaba-inc.com>
2015-07-03 02:14:08 +0800

    firt test

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
+    res.write('<h1>连续</h1>');
+    res.end('<p>我要玩转 node 与 mongodb !</p>');
 }).listen(port);
