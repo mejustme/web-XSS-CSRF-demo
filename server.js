f7bfc3096293c9e0562bce4e72cb90b2a5b93bd4
lianxu <qinhui.cqh@alibaba-inc.com>
2015-07-03 02:30:09 +0800

    html charset utf-8 not only txt utf-8

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>连续</h1>');
-    res.end('<p>我要玩转 node 与 mongodb !</p>');
+    res.write('')
+    res.write('<!doctype html>'+
+    '<html>'+
+    '<head>'+
+    '<meta charset="utf-8"/>'+
+    '<title>连续</title>'+
+    '</head>'+
+    '<body>'+
+    '<h1>我要玩转node与mongodb</h1>'+
+    '<h3>我是连续</h3>'+
+    '</body>'+
+    '</html>');
+    res.end();
 }).listen(port);
